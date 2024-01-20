922e4830e7be913906f240d5dac8ec70e19d9535
Rob Wilkinson <75864195+wilkyconsultants@users.noreply.github.com>
2024-01-19 20:44:03 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -16,8 +16,11 @@ struct ContentView: View {
     @State private var errorMessage = ""
     @State private var authenticated = false
     @State private var isLoading = false
+    @State private var navigateToTagList = false
 
     var body: some View {
+
+
         NavigationView {
             ZStack {
                 VStack {
@@ -38,7 +41,7 @@ struct ContentView: View {
                                     .autocapitalization(.none)
                                     .frame(height: 40)
                                 
-                                Button("Login \(errorMessage)") {
+                                Button("Login") {
                                     authenticate()
                                 }
                                 .disabled(authenticated)
@@ -70,11 +73,20 @@ struct ContentView: View {
                             
                             NavigationLink("⚠️ Tag Alerts", destination: TagDetailView(username: username,server: server, token: token ?? "", option: "LOST"))
                                 .foregroundColor(.blue)
+                            
+                            NavigationLink("Product Information", destination: InfoView())
+                                .foregroundColor(.blue)
+                            
+                            NavigationLink("Terms & Conditions", destination: TermsView())
+                                .foregroundColor(.red)
                         }
                     }
                     .padding()
                     .navigationTitle("👨🏻‍💼 MR🌐Tracker")
                 }
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text(""), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             }
         }
     }
@@ -137,24 +149,104 @@ struct ContentView: View {
                             self.authenticated = true
                         }
                     } else {
-                        errorMessage = " Failed"
+                        errorMessage = "Login Failed"
                         showAlert = true
                     }
                 } catch {
-                    errorMessage = " Failed"
+                    errorMessage = "Login Failed"
                     showAlert = true
                 }
             }.resume()
         } catch {
-            errorMessage = " Failed"
+            errorMessage = "Login Failed"
             showAlert = true
         }
     }
 }
 
+
 extension ContentView {
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+}
+
+struct TermsView: View {
+    let termsAndConditionsText = """
+    Terms and Conditions of Use
+
+    1. Acceptance of Terms
+
+    By using this product and associated services (hereinafter referred to as "the Product"), you agree to abide by these Terms and Conditions of Use. If you do not agree with any part of these terms, please do not use the Product.
+
+    2. Prohibition of Unauthorized Tracking
+
+    You expressly acknowledge and agree that the Product shall not, under any circumstances, be used for tracking any individual without their explicit and written consent. Tracking an individual without their direct and written consent is strictly prohibited.
+
+    3. Consent Requirement
+
+    Before utilizing the tracking features of the Product to monitor or trace any individual, you are obligated to obtain their express, informed, and written consent, specifying the extent and purpose of tracking. Failure to do so constitutes a breach of these Terms and Conditions.
+
+    4. Legal Responsibility
+
+    You understand and accept that by violating these Terms and Conditions and engaging in unauthorized tracking, you may be subject to legal actions, including but not limited to civil and criminal proceedings, as permitted by applicable laws. You agree to assume full legal responsibility for any consequences arising from such actions.
+
+    5. Compliance with Applicable Laws
+
+    You are required to comply with all local, state, federal, and international laws and regulations governing tracking and monitoring activities. The Product is not intended to be used for unlawful purposes.
+
+    6. Liability and Indemnification
+
+    By using the Product, you agree to hold harmless the product provider, its affiliates, employees, and agents from any liability, claims, losses, or damages arising from your use of the Product in violation of these Terms and Conditions.
+
+    7. Termination of Use
+
+    The product provider reserves the right to terminate your access to the Product or its services without notice if you engage in unauthorized tracking activities or otherwise violate these Terms and Conditions.
+
+    8. Amendments to Terms
+
+    These Terms and Conditions of Use may be amended by the product provider at any time. It is your responsibility to review them periodically for updates and changes.
+
+    9. Contact Information
+
+    If you have any questions or concerns regarding these Terms and Conditions or anything else, please contact us by using email address MrTracker.416@gmail.com, we would love to hear from you!
+    """
+
+    var body: some View {
+        ScrollView {
+            Text(termsAndConditionsText)
+                .padding()
+        }
+        .navigationBarTitle("Terms and Conditions", displayMode: .inline)
+    }
+}
+
+struct InfoView: View {
+    let InfoText = """
+    MR🌐Tracker Version 1.1
+    
+    The Ultimate Solution for Tracking Anything, Anywhere in the World.
+
+    MR🌐Tracker is a cutting-edge tracking solution, meticulously engineered to monitor the location of a diverse range of assets. From vacation cruises, letters and packages to luggage, vehicles, keys, backpacks, bicycles, and construction equipment – we've got you covered. Whether you're a traveler, a professional, or simply looking to safeguard your belongings, our advanced technology offers comprehensive oversight, providing the peace of mind you deserve.
+    
+    Simply decide what you want to track, attach a tag, and you're good to go – it's that simple.
+
+    Your typical setup includes a MacBook computer, iPhone, and tracking tags. Run as a franchise at your site. We handle all the necessary software installation, network setup, and offer remote support for a nominal charge. Rest assured about data privacy – all data uses SSL encryption, ensuring your information remains secure. Reports are accessible only to you as an admin, and you can grant access to additional users with user IDs chosen by you as the admin.
+
+    The tags record locations worldwide with no data charges.
+
+    If that's not enough MR🌐Tracker also does full inventory management, auction tracking and package tracking of your inventory, a fully integrated system, ideal for your Facebook coin, notes or precious metals side-hustle sales.
+
+    If you have any questions or concerns regarding Mr Tracker functionality please contact us by using email address MrTracker.416@gmail.com, we would love to hear from you!
+    """
+
+    var body: some View {
+        ScrollView {
+            Text(InfoText)
+                .padding()
+        }
+        .navigationBarTitle("Product Information", displayMode: .inline)
     }
 }
 
