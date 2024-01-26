0971248ec88ad14ed4b1cfbaef5f41425af99d86
Rob Wilkinson <75864195+wilkyconsultants@users.noreply.github.com>
2024-01-25 23:17:19 -0500

    Update _0240111_Example_login__switch_to_tag_type_list_with_countsApp.swift

--- 20240125/_0240111_Example_login__switch_to_tag_type_list_with_countsApp.swift
+++ 20240125/_0240111_Example_login__switch_to_tag_type_list_with_countsApp.swift
@@ -1,1 +1,15 @@
 
+//
+//  Created by Robert A Wilkinson on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct _0240111_Example_login__switch_to_tag_type_list_with_countsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
