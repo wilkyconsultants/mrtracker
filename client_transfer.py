de603a345538f0ef1cf5eb2bf0cb0d2d679ab694
Rob Wilkinson <75864195+wilkyconsultants@users.noreply.github.com>
2024-02-15 06:26:11 -0500

    Update client_transfer.py

--- client_transfer.py
+++ client_transfer.py
@@ -99,13 +99,16 @@ def process_data(data):
 #   Get the icloud ID
     icloud_ID="None"
     import subprocess
-    result = subprocess.run(["defaults", "read", "MobileMeAccounts", "Accounts"], capture_output=True, text=True)
-    output_lines = result.stdout.split('\n')
-    account_lines = [line for line in output_lines if "AccountID" in line]
-    account_ids = [line.split()[2] for line in account_lines]
-    cleaned_account_ids = [account_id.replace('"', '').replace(';', '') for account_id in account_ids]
-    for account_id in cleaned_account_ids:
-        icloud_ID=account_id
+    try:
+       result = subprocess.run(["defaults", "read", "MobileMeAccounts", "Accounts"], capture_output=True, text=True)
+       output_lines = result.stdout.split('\n')
+       account_lines = [line for line in output_lines if "AccountID" in line]
+       account_ids = [line.split()[2] for line in account_lines]
+       cleaned_account_ids = [account_id.replace('"', '').replace(';', '') for account_id in account_ids]
+       for account_id in cleaned_account_ids:
+           icloud_ID=account_id
+    except:
+           icloud_ID = "Error"
 #
     results = []
     for item in data:
